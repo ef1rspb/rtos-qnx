00cecadcb530b5ce6543cf0993286276dbbbe37b
Sasha Malina <ef1rspb@gmail.com>
2018-09-24 22:07:58 +0300

    Implement Exercise 1

---
+++ practical-lesson-6/src/practical-lesson-6.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int k,a,b;
+	for (k=1;k<4; k++)
+	{
+		printf("Hello, world!\n");
+		a = k + 10;
+		b = 2 * a;
+	}
+	return EXIT_SUCCESS;
+}
