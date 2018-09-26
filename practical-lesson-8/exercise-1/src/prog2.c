639900f64b32d41dc3a5b0c0a19b6747cefd18c3
Sasha Malina <ef1rspb@gmail.com>
2018-09-26 18:21:42 +0300

    Implement Exercise 1 of lesson 8

---
+++ practical-lesson-8/exercise-1/src/prog2.c
@@ -0,0 +1,212 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+#include <sys/neutrino.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "roby.h"
+
+#define ATTACH_POINT "apu/roby"
+#define COMMANDS_MAX_COUNT 10
+
+// пересылаемое сообщение
+struct MESSAGE
+{
+    // message type
+    unsigned char type;
+    // send data
+    unsigned int buf;
+};
+
+struct PULSE
+{
+	unsigned int code;
+	unsigned int value;
+};
+
+int coid;
+
+void *Display() {
+    struct MESSAGE msg;
+    struct PULSE pulse;
+    int chid, x_cnt, y_cnt, z_cnt;
+    x_cnt = y_cnt = z_cnt = 0;
+    int changed = 0;
+    chid = ChannelCreate(0);
+    unsigned char rmsg;
+    printf("Создан канал %d\n ", chid);
+
+    // Инициализация датчиков координат X,Y,Z
+    for(int i = 4; i < 7; i++)
+    {
+        msg.type = i;
+        msg.buf = chid;
+        MsgSend(coid, &msg, sizeof(msg), NULL, 0);
+    }
+
+    for(;;)
+    {
+    	sleep(1);
+        MsgReceivePulse(chid, &pulse, sizeof(pulse), NULL);
+        printf("%u ", pulse.code);
+
+        switch(pulse.code)
+        {
+        case B_X:                       // Если изменилась координата X.
+            x_cnt = pulse.value;
+            changed = 1;
+            break;
+
+        case B_Y:                       // Если изменилась координата Y.
+            y_cnt = pulse.value;
+            changed = 1;
+            break;
+
+        case B_Z:                       // Если изменилась координата Z.
+            z_cnt = pulse.value;
+            changed = 1;
+            break;
+
+        default:                        // Если изменились другие координаты.
+        	changed = 0;
+            break;
+        }
+
+        if(changed)
+        {
+        	// Вывод текущих координат на экран.
+        	        printf("Roby's coordinates:\r\n\tX=%d\r\n\tY=%d\r\n\tZ=%d\r\n",
+        	            x_cnt,
+        	            y_cnt,
+        	            z_cnt);
+        }
+    }
+}
+
+int main()
+{
+    unsigned char PA=0, PC=0; //Эти переменные хранят значения регистров PA и PC
+    struct MESSAGE msg; // буфер посылаемого сообщения
+    unsigned char rmsg; // Буфер ответного сообщения
+    char command, ch[3];
+
+    coid = name_open(ATTACH_POINT, 0);
+    if(coid == -1)
+    {
+        return EXIT_FAILURE;
+    }
+
+    printf("apu/roby has coid = %d\n", coid);
+
+    pthread_create(NULL, NULL, &Display, NULL);
+
+    do
+    {
+        printf("Enter command>\n");
+        scanf("%s", ch);
+
+        switch(ch[0])
+        {
+        case 'I':
+            // Двигаться в начальное положение по всем координатам
+            // и сбросить значения датчиков положений.
+            PA = 0; PC = 0; //Для этого сбросить в 0 переменные PA и PC.
+            command ='C'; //Подать команду записи в регистр PC роботу Roby.
+            break;
+
+        case '+': case '=':
+                command = 'c';
+                break;
+        case '!':
+                command = 'B';
+                break;
+
+        case 'Y':
+            scanf("%s", ch);
+            switch(ch[0])
+            {
+            case 'X':
+                PA ^= A_X_FORWARD;
+                command ='A';
+                break;
+            case 'x':
+                PA ^= A_X_BACK;
+                command ='A';
+                break;
+            case 'Y':
+                PA ^= A_Y_FORWARD;
+                command ='A';
+                break;
+            case 'y':
+                PA ^= A_Y_BACK;
+                command ='A';
+                break;
+            case 'Z':
+                PA ^= A_Z_FORWARD;
+                command ='A';
+                break;
+            case 'z':
+                PA ^= A_Z_BACK;
+                command ='A';
+                break;
+            case 'F':
+                PC ^= C_F_FORWARD;
+                command ='C';
+                break;
+            case 'f':
+                PC ^= C_F_BACK;
+                command ='C';
+                break;
+            case 'W':
+                PC ^= C_W_FORWARD;
+                command ='C';
+                break;
+            case 'w':
+                PC ^= C_W_BACK;
+                command ='C';
+                break;
+            case 'S':
+                PA ^= A_S;
+                command ='A';
+                break;
+            case 'D':
+                PA ^= A_D;
+                command ='A';
+                break;
+            }
+
+        }
+
+        switch(command)
+            {
+            case 'A':
+                msg.buf = PA;
+                msg.type = 0;
+                MsgSend(coid, &msg, sizeof(msg), NULL, 0);
+                break;
+            case 'B':
+                msg.type = 3;
+                MsgSend(coid, &msg, sizeof(msg), &rmsg, sizeof(rmsg));
+                printf("Port B: 0x%X\n", rmsg);
+                break;
+            case 'c':
+                msg.type = 2;
+                MsgSend(coid, &msg, sizeof(msg), &rmsg, sizeof(rmsg));
+                printf("Port C: 0x%X\n", rmsg);
+                break;
+            case 'C':
+                msg.type = 1;
+                msg.buf = PC;
+                MsgSend(coid, &msg, sizeof(msg), NULL, 0);
+                break;
+            default:
+                printf("default last\n");
+                    break;
+            }
+
+    }  while(1);
+
+}
---
+++ practical-lesson-8/exercise-1/src/roby.h
@@ -0,0 +1,62 @@
+/*
+ * roby.h
+ *
+ *  Created on: Sep 19, 2018
+ *      Author: sasha
+ */
+
+#ifndef ROBY_H_
+#define ROBY_H_
+
+//¨âë à¥£¨áâà  A
+#define  A_D            0x01
+#define  A_S            0x02
+#define  A_X_FORWARD    0x04
+#define  A_X_BACK       0x08
+#define  A_Z_BACK       0x10
+#define  A_Z_FORWARD    0x20
+#define  A_Y_BACK       0x40
+#define  A_Y_FORWARD    0x80
+
+//¨âë à¥£¨áâà  B
+#define  B_X            0x01
+#define  B_Y            0x02
+#define  B_Z            0x04
+#define  B_W_END        0x08
+#define  B_W_BEGIN      0x10
+#define  B_Z_BEGIN      0x20
+#define  B_Y_BEGIN      0x40
+#define  B_X_BEGIN      0x80
+
+//¨âë à¥£¨áâà  C
+#define  C_F_END        0x04
+#define  C_F_BEGIN      0x08
+#define  C_W_FORWARD    0x10
+#define  C_W_BACK       0x20
+#define  C_F_FORWARD    0x40
+#define  C_F_BACK       0x80
+
+
+#define  X_MIN          0
+#define  X_MAX          1024
+#define  Y_MIN          0
+#define  Y_MAX          1024
+#define  Z_MIN          0
+#define  Z_MAX          1024
+#define  W_MIN          0
+#define  W_MAX          100
+#define  F_MIN          0
+#define  F_MAX          50
+#define  S_MIN          0
+#define  S_MAX          1
+#define  D_MIN          0
+#define  D_MAX          1
+
+#define W_END           0x08
+#define W_BEGIN         0x10
+#define F_END           0x20
+#define F_BEGIN         0x40
+
+
+
+#endif /* ROBY_H_ */
